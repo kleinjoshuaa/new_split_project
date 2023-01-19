439f19360074df5db414c89d10eb2470ad1a66bb
Joshua Klein <klein.joshua.a@gmail.com>
2023-01-19 10:48:57 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# new_split_project
+# Workspace Generator
+
+This is a tool that lets you create new split workspaces just with a simple form. 
+
+
+Enhancements could be made for error handling - right now if there are any issues with creating the environments or trafficTypes it doesn't show them back to the user. 
+
+
+Install with `npm install` and ensure that a .env file exists with your API_KEY and ORG_ID strings in it. Once there, run `node index.js` and it will serve on port 3201. 
+
+Enjoy
--- index.js
+++ index.js
@@ -13,65 +13,61 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const ADMIN_API_KEY = enVars.API_KEY;
 const ORG_ID = enVars.ORG_ID;
 
-
-
 async function createEnv(name, wsId, prefix, isProd) {
-    var data = JSON.stringify({
-        name: `${prefix}-${name}`,
-        production: isProd,
-        changePermissions: {
-          allowKills: false,
-          areApprovalsRequired: false,
-          areApproversRestricted: false,
-          areEditorsRestricted: false,
-        },
-        dataExportPermissions: {
-          areExportersRestricted: false,
-        },
-      });
+  var data = JSON.stringify({
+    name: `${prefix}-${name}`,
+    production: isProd,
+    changePermissions: {
+      allowKills: false,
+      areApprovalsRequired: false,
+      areApproversRestricted: false,
+      areEditorsRestricted: false,
+    },
+    dataExportPermissions: {
+      areExportersRestricted: false,
+    },
+  });
 
-      var config = {
-        method: "post",
-        url: `https://api.split.io/internal/api/v2/environments/ws/${wsId}`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${ADMIN_API_KEY}`,
-        },
-        data: data,
-      };
+  var config = {
+    method: "post",
+    url: `https://api.split.io/internal/api/v2/environments/ws/${wsId}`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${ADMIN_API_KEY}`,
+    },
+    data: data,
+  };
   try {
-     let response = await axios(config)
-     return response;
-    } catch (error) {
-        return error
-    }
+    let response = await axios(config);
+    return {success: true};
+  } catch (error) {
+    return {success: false, code: error.response.data.code , msg: error.response.data.message};
+  }
 }
 
 async function createTT(ttName, wsId) {
-    var data = JSON.stringify({
-        "name": ttName
-      });
-      
-      var config = {
-        method: 'post',
-        url: `https://api.split.io/internal/api/v2/trafficTypes/ws/${wsId}`,
-        headers: { 
-          'Content-Type': 'application/json', 
-          'Authorization': `Bearer ${ADMIN_API_KEY}`
-        },
-        data : data
-      };
-    try {
-       let response =  await axios(config)
-       return response
-    } catch(error) {
-        return error
-    }
+  var data = JSON.stringify({
+    name: ttName,
+  });
+
+  var config = {
+    method: "post",
+    url: `https://api.split.io/internal/api/v2/trafficTypes/ws/${wsId}`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${ADMIN_API_KEY}`,
+    },
+    data: data,
+  };
+  try {
+    let response = await axios(config);
+    return {success: true}
+  } catch (error) {
+    return {success: false, code: error.response.data.code , msg: error.response.data.message};
+  }
 }
 
 app.use(express.static(path.join(__dirname, "public")));
-
-
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -79,7 +75,6 @@ app.get("/", (req, res) => {
 
 app.post("/createWS", (req, res) => {
   const values = req.body;
-  console.log(JSON.stringify(values));
   var data = JSON.stringify({
     name: values.team,
     requiresTitleAndComments:
@@ -101,16 +96,24 @@ app.post("/createWS", (req, res) => {
   axios(config)
     .then(function (workspaces) {
       //console.log(workspaces);
-      values.environments.forEach(function(v) {
-        let isProd = false;
-        if(v == "Prod") {
-            isProd = true
+        if (typeof values.environments !== "undefined") {
+          values.environments.forEach(function (v) {
+            let isProd = false;
+            if (v == "Prod") {
+              isProd = true;
+            }
+            createEnv(workspaces.data.name, workspaces.data.id, v, isProd)
+       
+        });
         }
-        createEnv(workspaces.data.name, workspaces.data.id, v, isProd)
-      })
-      values.traffictypes.forEach(function (v) {
-        createTT(v, workspaces.data.id)
-      })
+        if (typeof values.traffictypes !== "undefined") {
+          values.traffictypes.forEach(function (v) {
+            let result = createTT(v, workspaces.data.id);
+
+          });
+        }
+   
+
       res.render("result", {
         ORGID: ORG_ID,
         WSID: workspaces.data.id,
@@ -119,13 +122,13 @@ app.post("/createWS", (req, res) => {
         ENVS: values.environments.join(", "),
         TTS: values.traffictypes.concat(["user"].join(", ")),
       });
+    
     })
-    .catch(function(error) {
-        console.log(error)
-        res.render("error", {
-                ERRMSG: error.response.data.message,
-                ERRCODE: error.response.data.code,
-            })
+    .catch(function (error) {
+      res.render("error", {
+        ERRMSG: error.response.data.message,
+        ERRCODE: error.response.data.code,
+      });
     });
 });
 
